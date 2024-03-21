df3d84391ce6fddc916426c60fa3206ccf824f0a
jeandut <jeandut@users.noreply.github.com>
2024-03-21 15:19:10 +0000

    deploy: bf5d6ebc9aa7d82d9a498434483ea5b31906e67d

---
+++ _static/documentation_options.js
@@ -0,0 +1,14 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'bf5d6ebc9aa7d82d9a498434483ea5b31906e67d',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
