25e838d48c8d1f3a51a7d7acc91771477ea2f8f4
Honghao Li <honghao.li@owkin.com>
2024-04-19 15:49:15 +0200

    test: add "test_run" experiment, include fedeca in the test

--- fedeca/tests/test_run_experiment.py
+++ fedeca/tests/test_run_experiment.py
@@ -9,20 +9,20 @@ from fedeca.utils.experiment_utils import load_dataframe_from_pickles
 
 
 def test_example_experiment(tmp_path):
-    """Test hydra run with the example experiment."""
+    """Test hydra run with the "test_run" experiment."""
     subprocess.run(
         [
             "python",
             "-m",
             "experiments.run_experiment",
             "-m",
-            "experiment=example",
+            "experiment=test_run",
             f"hydra.sweep.dir={tmp_path}",
         ],
         check=False,
     )
 
-    df_res = load_dataframe_from_pickles(tmp_path / "results_Example_experiment.pkl")
+    df_res = load_dataframe_from_pickles(tmp_path / "results_Test_run.pkl")
     df_res = df_res.drop(columns=["fit_time"]).reset_index(drop=True)
     # Not ideal, but save to and load from csv file to avoid dtype issues
     path_csv = tmp_path / "results.csv"
@@ -30,7 +30,7 @@ def test_example_experiment(tmp_path):
     df_res = pd.read_csv(path_csv)
 
     path_csv_true = (
-        Path(__file__).parent / "artifacts/results_run_experiment_example.csv"
+        Path(__file__).parent / "artifacts/results_test_run.csv"
     )
     df_true = pd.read_csv(path_csv_true)
 
