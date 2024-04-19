0fe642ded260df32917099bd035a450772361593
Honghao <honghao.li@owkin.com>
2024-04-19 18:24:23 +0200

    refactor: synthetic experiment pipeline (#31)
    * relocate function `single_experiment`
    
    * fix: lint error
    
    * instantiate models inside `single_experiment`
    
    * update docstring
    
    * fix linting issue
    
    * test: pipeline with example experiment
    
    * test: add "test_run" experiment, include fedeca in the test

--- experiments/run_experiment.py
+++ experiments/run_experiment.py
@@ -1,15 +1,15 @@
 """Main module for running hydra config based experiments."""
+
 import pickle
 import re
-from collections.abc import Mapping
 
 import hydra
 import numpy as np
 from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig, open_dict
 
-from fedeca.utils.experiment_utils import single_experiment
-from fedeca.utils.survival_utils import BaseSurvivalEstimator, CoxData
+from experiments.synthetic import single_experiment
+from fedeca.utils.survival_utils import CoxData
 
 
 @hydra.main(version_base=None, config_path="config", config_name="default_config")
@@ -37,12 +37,7 @@ def run_experiment(cfg: DictConfig):
     output = f"{config_hydra.sweep.dir}/results_{output}.pkl"
 
     data_gen: CoxData = hydra.utils.instantiate(cfg.data)
-    models: Mapping[str, BaseSurvivalEstimator] = dict(
-        (name, hydra.utils.instantiate(model)) for name, model in cfg.models.items()
-    )
-    for model in models.values():
-        model.set_random_state(data_gen.rng)
-    if "fit_fedeca" in cfg.keys():
+    if "fit_fedeca" in cfg:
         fedeca_config = hydra.utils.instantiate(cfg.fit_fedeca)
     else:
         fedeca_config = None
@@ -51,7 +46,7 @@ def run_experiment(cfg: DictConfig):
         single_experiment(
             data_gen,
             n_samples=cfg.parameters.n_samples,
-            models=models,
+            model_configs=cfg.models,
             treated_col=models_common.treated_col,
             event_col=models_common.event_col,
             duration_col=models_common.duration_col,
