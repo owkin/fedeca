Fixing weighted SMD computation both in pooled and FL (#50)
* trying to fix weighted SMD computation

* trying to fix weighted SMD computation

* trying to fix weighted SMD computation

* fixing metrics

* matching formulas

* matching cobalt

* matching cobalt

* first trial at fixing things

* trying to fix stuff

* fixing forgotten mask

* trying...

* fixing various stuff

* everything should be passing

* linting

* linting v2

fedeca/metrics/metrics.py
@@ -4,7 +4,9 @@ import numpy as np
 import pandas as pd
 
 
-def standardized_mean_diff(confounders, treated):
+def standardized_mean_diff(
+    confounders, treated, weights=None, use_unweighted_variance=True
+):
     """Compute the Standardized Mean Differences (SMD).
 
     Compute the Standardized Mean Differences between
@@ -12,47 +14,133 @@ def standardized_mean_diff(confounders, treated):
 
     Parameters
     ----------
-    confounders : np.ndarray
+    confounders : pd.DataFrame
         confounders array.
     treated : np.ndarray
         mask of booleans giving information about treated patients.
+    weights : np.ndarray
+        weights for the aggregation
+    use_unweighted_variance : bool
+        if True, the variance is computed without weights. To follow
+        https://stats.stackexchange.com/questions/618643/formula-for-standardized-mean-difference-in-cobalt-package-for-categorical-varia  # noqa: E501
+        If False use recalibrated variance as in https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4626409/.  # noqa: E501
 
     Returns
     -------
     smd: np.ndarray
         standardized mean differences of the confounders.
     """
+    if weights is None:
+        weights = np.ones((len(confounders.index)))
+
+    if use_unweighted_variance:
+        weights_var = np.ones((weights.shape[0]))
+    else:
+        weights_var = weights
+
+    # unbiased var estimator see https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4626409/  # noqa: E501
+    # when use_unweighted_variance is True this just is n / (n - 1) for unbiasedness
+    var_scaler_treated = weights_var[treated].sum() ** 2 / (
+        weights_var[treated].sum() ** 2 - np.power(weights_var[treated], 2).sum()
+    )
+    var_scaler_untreated = weights_var[~treated].sum() ** 2 / (
+        weights_var[~treated].sum() ** 2 - np.power(weights_var[~treated], 2).sum()
+    )
+
     n_unique = confounders.nunique()
     cat_variables = n_unique == 2
     continuous_variables = n_unique != 2
 
-    smd_continuous = (
-        confounders.loc[treated, continuous_variables].mean()
-        - confounders.loc[~treated, continuous_variables].mean()
+    cont_columns = confounders.columns[continuous_variables].tolist()
+    cat_columns = confounders.columns[cat_variables].tolist()
+
+    treated_confounders_avg = np.average(
+        confounders.loc[treated, cont_columns], weights=weights[treated], axis=0
     )
-    smd_continuous /= np.sqrt(
-        (
-            confounders.loc[treated, continuous_variables].var()
-            + confounders.loc[~treated, continuous_variables].var()
-        )
-        / 2
+    untreated_confounders_avg = np.average(
+        confounders.loc[~treated, cont_columns],
+        weights=weights[~treated],
+        axis=0,
     )
+
+    # Returning back to a Series object as would .mean() do
+    smd_continuous = pd.Series(
+        treated_confounders_avg - untreated_confounders_avg, index=cont_columns
+    )
+    smd_continuous /= pd.Series(
+        np.sqrt(
+            (
+                var_scaler_treated
+                * np.average(
+                    np.power(
+                        confounders.loc[treated, cont_columns]
+                        - np.average(
+                            confounders.loc[treated, cont_columns],
+                            weights=weights_var[treated],
+                            axis=0,
+                        ),
+                        2,
+                    ),
+                    weights=weights_var[treated],
+                    axis=0,
+                )
+                + var_scaler_untreated
+                * np.average(
+                    np.power(
+                        confounders.loc[~treated, cont_columns]
+                        - np.average(
+                            confounders.loc[~treated, cont_columns],
+                            weights=weights_var[~treated],
+                            axis=0,
+                        ),
+                        2,
+                    ),
+                    weights=weights_var[~treated],
+                    axis=0,
+                )
+            )
+            / 2
+        ),
+        index=cont_columns,
+    )
+
     smd_continuous *= 100
 
-    smd_cat = (
-        confounders.loc[treated, cat_variables].mean()
-        - confounders.loc[~treated, cat_variables].mean()
+    treated_cat_confounders_avg = np.average(
+        confounders.loc[treated, cat_columns], weights=weights[treated], axis=0
     )
-    smd_cat /= np.sqrt(
-        (
-            confounders.loc[treated, cat_variables].mean()
-            * (1 - confounders.loc[treated, cat_variables]).mean()
-            + confounders.loc[~treated, cat_variables].mean()
-            * (1 - confounders.loc[~treated, cat_variables]).mean()
-        )
-        / 2
+    untreated_cat_confounders_avg = np.average(
+        confounders.loc[~treated, cat_columns], weights=weights[~treated], axis=0
+    )
+
+    smd_cat = pd.Series(
+        treated_cat_confounders_avg - untreated_cat_confounders_avg, index=cat_columns
+    )
+    # TODO check if scaling of variance is correct
+    smd_cat /= pd.Series(
+        np.sqrt(
+            (
+                var_scaler_treated
+                * treated_cat_confounders_avg
+                * np.average(
+                    1 - confounders.loc[treated, cat_columns],
+                    weights=weights_var[treated],
+                    axis=0,
+                )
+                + var_scaler_untreated
+                * untreated_cat_confounders_avg
+                * np.average(
+                    1 - confounders.loc[~treated, cat_columns],
+                    weights=weights_var[~treated],
+                    axis=0,
+                )
+            )
+            / 2
+        ),
+        index=cat_columns,
     )
     smd_cat *= 100
 
     smd = pd.concat([smd_continuous, smd_cat])
+    smd = smd.reindex(confounders.columns)
     return smd

fedeca/strategies/fed_smd.py
@@ -34,6 +34,7 @@ class FedSMD(ComputePlanBuilder):
         treated_col: str,
         propensity_model: torch.nn.Module,
         client_identifier: str,
+        use_unweighted_variance: bool = True,
         tol: float = 1e-16,
     ):
         """Initialize FedSMD strategy.
@@ -49,6 +50,10 @@ class FedSMD(ComputePlanBuilder):
 
         propensity_model : Union[None, nn.Module], optional
             _description_, by default None
+        client_identifier : str
+            _description_
+        use_unweighted_variance : bool, optional
+            _description_, by default True
         """
         super().__init__()
 
@@ -59,6 +64,7 @@ class FedSMD(ComputePlanBuilder):
         self._propensity_model = propensity_model
         self._propensity_fit_cols = None
         self._client_identifier = client_identifier
+        self._use_unweighted_variance = use_unweighted_variance
         self._tol = tol
         self.statistics_result = None
 
@@ -68,6 +74,7 @@ class FedSMD(ComputePlanBuilder):
         self.kwargs["treated_col"] = treated_col
         self.kwargs["propensity_model"] = propensity_model
         self.kwargs["client_identifier"] = client_identifier
+        self.kwargs["use_unweighted_variance"] = use_unweighted_variance
         self.kwargs["tol"] = tol
 
     def build_compute_plan(
@@ -176,7 +183,6 @@ class FedSMD(ComputePlanBuilder):
             "iptw",
         )
         # X contains only the treatment column (strategy == iptw)
-
         X, _, weights = compute_X_y_and_propensity_weights_function(
             X, y, treated, Xprop, self._propensity_model, self._tol
         )
@@ -185,29 +191,51 @@ class FedSMD(ComputePlanBuilder):
         # we use Xprop which contain all propensity columns, which
         # are the only ones we are interested in
         raw_data = pd.DataFrame(Xprop, columns=propensity_cols)
-        weighted_data = pd.DataFrame(
-            np.multiply(weights, Xprop), columns=propensity_cols
+
+        weights_df = pd.DataFrame(
+            np.repeat(weights, Xprop.shape[1], axis=1), columns=propensity_cols
         )
+
         results = {}
         for treatment in [0, 1]:
             mask_treatment = treated == treatment
             res_name = "treated" if treatment else "untreated"
             results[res_name] = {}
+            # Here we pass weights
             results[res_name]["weighted"] = {
                 f"moment{k}": compute_uncentered_moment(
-                    weighted_data[mask_treatment], k
+                    raw_data[mask_treatment], k, weights[mask_treatment]
                 )
                 for k in range(1, 3)
             }
+            # Here we don't
             results[res_name]["unweighted"] = {
                 f"moment{k}": compute_uncentered_moment(raw_data[mask_treatment], k)
                 for k in range(1, 3)
             }
-            results[res_name]["unweighted"]["n_samples"] = results[res_name][
-                "weighted"
-            ]["n_samples"] = (
+            # Here we compute aggregated effective sample size (ess)
+            results[res_name]["unweighted"]["n_samples"] = (
                 raw_data[mask_treatment].select_dtypes(include=np.number).count()
             )
+            results[res_name]["weighted"]["n_samples"] = (
+                weights_df[mask_treatment].select_dtypes(include=np.number).sum()
+            )
+            if not self._use_unweighted_variance:
+                # We add these numbers for scaling variance as in https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4626409/  # noqa: E501
+                results[res_name]["weighted"]["weights"] = {}
+                results[res_name]["weighted"]["weights"]["sum_moment1"] = (
+                    weights_df[mask_treatment]
+                    .select_dtypes(include=np.number)
+                    .sum(axis=0)
+                )
+                # Here we compute squared weights sum
+                results[res_name]["weighted"]["weights"]["sum_moment2"] = (
+                    weights_df[mask_treatment]
+                    .pow(2)
+                    .select_dtypes(include=np.number)
+                    .sum(axis=0)
+                )
+
         return results
 
     @remote
@@ -231,33 +259,69 @@ class FedSMD(ComputePlanBuilder):
         def std_mean_differences(x, y):
             """Compute standardized mean differences."""
             means_x = x["global_uncentered_moment_1"]
-            # we match nump std with 0 ddof contrary to standarization for Cox
-            stds_x = np.sqrt(x["global_centered_moment_2"] + self._tol)
-
             means_y = y["global_uncentered_moment_1"]
-            # we match nump std with 0 ddof contrary to standarization for Cox
-            stds_y = np.sqrt(y["global_centered_moment_2"] + self._tol)
-
+            var_x = x["global_centered_moment_2"]
+            var_y = y["global_centered_moment_2"]
             smd_df = means_x.subtract(means_y).div(
-                stds_x.pow(2).add(stds_y.pow(2)).div(2).pow(0.5)
+                var_x.add(var_y).div(2).pow(0.5).add(self._tol)
             )
             return smd_df
 
+        # First we compute means and vars wo weights
         treated_raw = compute_global_moments(
             [shared_state["treated"]["unweighted"] for shared_state in shared_states]
         )
         untreated_raw = compute_global_moments(
             [shared_state["untreated"]["unweighted"] for shared_state in shared_states]
         )
-
+        # We use directly to compute the SMD before propensity weighting
         smd_raw = std_mean_differences(treated_raw, untreated_raw)
 
+        # Then we compute means and vars WITH WEIGHTS
         treated_weighted = compute_global_moments(
             [shared_state["treated"]["weighted"] for shared_state in shared_states]
         )
         untreated_weighted = compute_global_moments(
             [shared_state["untreated"]["weighted"] for shared_state in shared_states]
         )
+        if not self._use_unweighted_variance:
+            # we compute the var scaler for treated population
+            def compute_var_scaler(weights_per_client):
+                total_weighted_n_samples = sum(
+                    [s["sum_moment1"].iloc[0] for s in weights_per_client]
+                )
+                total_weighted_n_samples_squared = sum(
+                    [s["sum_moment2"].iloc[0] for s in weights_per_client]
+                )
+                return total_weighted_n_samples**2 / (
+                    total_weighted_n_samples**2 - total_weighted_n_samples_squared
+                )
+
+            var_scaler_treated = compute_var_scaler(
+                [
+                    shared_state["treated"]["weighted"]["weights"]
+                    for shared_state in shared_states
+                ]
+            )
+            treated_weighted["global_centered_moment_2"] *= var_scaler_treated
+
+            # we compute the var scaler for untreated population
+            var_scaler_untreated = compute_var_scaler(
+                [
+                    shared_state["untreated"]["weighted"]["weights"]
+                    for shared_state in shared_states
+                ]
+            )
+
+            untreated_weighted["global_centered_moment_2"] *= var_scaler_untreated
+
+        else:
+            treated_weighted["global_centered_moment_2"] = treated_raw[
+                "global_centered_moment_2"
+            ]
+            untreated_weighted["global_centered_moment_2"] = untreated_raw[
+                "global_centered_moment_2"
+            ]
 
         smd_weighted = std_mean_differences(treated_weighted, untreated_weighted)
 

fedeca/tests/strategies/test_smd.py
@@ -4,7 +4,6 @@ import subprocess
 from pathlib import Path
 
 import git
-import numpy as np
 import pandas as pd
 import torch
 from substrafl.dependency import Dependency
@@ -28,7 +27,9 @@ class TestSMD(TestTempDir):
     Tests the FL computation of SMD is the same as in pandas-pooled version
     """
 
-    def setUp(self, backend_type="subprocess", ndim=10) -> None:
+    def setUp(
+        self, backend_type="subprocess", ndim=10, use_unweighted_variance=True
+    ) -> None:
         """Set up the quantities needed for the tests."""
         # Let's generate 1000 data samples with 10 covariates
         data = CoxData(seed=42, n_samples=1000, ndim=ndim)
@@ -78,6 +79,7 @@ class TestSMD(TestTempDir):
                 size=self.propensity_model.fc1.bias.data.shape, dtype=torch.float64
             )
         )
+        self.use_unweighted_variance = use_unweighted_variance
 
     def test_end_to_end(self):
         """Compare a FL and pooled computation of Moments.
@@ -91,6 +93,7 @@ class TestSMD(TestTempDir):
             event_col="event",
             propensity_model=self.propensity_model,
             client_identifier="center",
+            use_unweighted_variance=self.use_unweighted_variance,
         )
 
         compute_plan = execute_experiment(
@@ -124,29 +127,44 @@ class TestSMD(TestTempDir):
         weights = self.df["treatment"] * 1.0 / propensity_scores + (
             1 - self.df["treatment"]
         ) * 1.0 / (1.0 - propensity_scores)
-        weights = weights.values
+        weights = weights.values.flatten()
 
-        X_weighted = (Xprop * weights[:, np.newaxis]).numpy()
-        X_weighted_df = pd.DataFrame(X_weighted, columns=covariates)
         X_df = pd.DataFrame(Xprop.numpy(), columns=covariates)
 
         standardized_mean_diff_pooled_weighted = standardized_mean_diff(
-            X_weighted_df,
+            X_df,
             self.df["treatment"] == 1,
+            weights=weights,
+            use_unweighted_variance=self.use_unweighted_variance,
         ).div(100.0)
         standardized_mean_diff_pooled_unweighted = standardized_mean_diff(
             X_df,
             self.df["treatment"] == 1,
+            use_unweighted_variance=self.use_unweighted_variance,
         ).div(100.0)
 
         # We check equality of FL computation and pooled results
-        pd.testing.assert_series_equal(
-            standardized_mean_diff_pooled_weighted,
-            fl_results["weighted_smd"],
-            rtol=1e-2,
-        )
         pd.testing.assert_series_equal(
             standardized_mean_diff_pooled_unweighted,
             fl_results["unweighted_smd"],
             rtol=1e-2,
         )
+        pd.testing.assert_series_equal(
+            standardized_mean_diff_pooled_weighted,
+            fl_results["weighted_smd"],
+            rtol=1e-2,
+        )
+
+
+class TestSMDWeightedVar(TestSMD):
+    """Test SMD with weighted variance.
+
+    Parameters
+    ----------
+    TestSMD : _type_
+        _description_
+    """
+
+    def setUp(self) -> None:
+        """Set SMD test with weighted variance."""
+        super().setUp(use_unweighted_variance=False)

fedeca/utils/moments_utils.py
@@ -6,7 +6,7 @@ import pandas as pd
 from scipy.special import binom
 
 
-def compute_uncentered_moment(data, order):
+def compute_uncentered_moment(data, order, weights=None):
     """Compute the uncentered moment.
 
     Parameters
@@ -15,6 +15,8 @@ def compute_uncentered_moment(data, order):
         dataframe.
     order : int
         order of the moment.
+    weights : np.array
+        weight for the aggregation.
 
     Returns
     -------
@@ -26,10 +28,23 @@ def compute_uncentered_moment(data, order):
     NotImplementedError
         Raised if the data type is not Dataframe nor np.ndarray.
     """
+    if weights is None:
+        weights = np.ones(len(data.index))
+
+    # TODO categorical variables need a special treatment
+
     if isinstance(data, (pd.DataFrame, pd.Series)):
-        moment = data.select_dtypes(include=np.number).pow(order).mean(skipna=True)
+        moment = data.select_dtypes(include=np.number).pow(order)
+        moment = moment.mul(weights, axis=0)
+        moment = moment.sum(skipna=True)
+        moment /= weights.sum()
+
     elif isinstance(data, np.ndarray):
-        moment = np.nanmean(np.power(data, order), axis=0)
+        moment = np.power(data, order)
+        moment = np.multiply(moment, weights)
+        moment = np.sum(moment, axis=0)
+        moment /= weights.sum()
+
     else:
         raise NotImplementedError(
             "Only DataFrame or numpy array are currently handled."
@@ -130,17 +145,21 @@ def compute_global_moments(shared_states):
         for k in range(1, 2 + 1)
     ]
     n_samples = sum([s["n_samples"].iloc[0] for s in shared_states])
+
     results = {
         f"global_centered_moment_{k}": compute_centered_moment(
             tot_uncentered_moments[:k]
         )
         for k in range(1, 2 + 1)
     }
+
     results.update(
         {
             f"global_uncentered_moment_{k+1}": moment
             for k, moment in enumerate(tot_uncentered_moments)
         }
     )
+
     results.update({"total_n_samples": n_samples})
+
     return results