bc4f338225f26fc0d6e2e4f67a12460c234b16bd
jeandut <jean.du-terrail@owkin.com>
2025-01-20 15:18:05 +0100

    fixing stuff

--- experiments/pdac/create_pairs_tables.py
+++ experiments/pdac/create_pairs_tables.py
@@ -34,21 +34,22 @@ for pickle_file in results_exchangeability:
         current_res = pickle.load(pick)
 
     current_train_datasets = current_res["kwargs"]["train_data_nodes"]
+
     current_train_datasets_names = [owkin_ds.get_dataset(dataset.data_manager_key).name for dataset in current_train_datasets]
     sizes = [int(name.split("_")[-2][1:]) for name in current_train_datasets_names]
     id_pop_treatment = current_train_datasets_names[0][:4]
     centers = [name.split("_")[0] for name in current_train_datasets_names]
     assert all([c in list(names_mapping.keys()) for c in centers])
     centers = [names_mapping[c] for c in centers]
-
-    current_results = current_res["bootstrap"]["results"]
-    f.write(
-        f"FedECA (bootstrap) & "
-        f"\\scriptsize{{{centers[0]}, {centers[1]}}} & "
-        f"\\num{{{current_results['exp(coef)'].item()}}}"
-        f"(\\num{{{current_results['exp(coef) lower 95%'].item()}}}, \\num{{{current_results['exp(coef) upper 95%'].item()}}}) & "
-        f"\\num{{{current_results['z'].item()}}} & \\num[round-mode=figures,round-precision=3]{{{current_results['p'].item()}}} \\\\ \n"
-    )
+    for variance_method in ["bootstrap", "robust", "naïve"]:
+        current_results = current_res[variance_method]["results"]
+        f.write(
+            f"FedECA ({variance_method}) & "
+            f"\\scriptsize{{{centers[0]}, {centers[1]}}} & "
+            f"\\num{{{current_results['exp(coef)'].item()}}}"
+            f"(\\num{{{current_results['exp(coef) lower 95%'].item()}}}, \\num{{{current_results['exp(coef) upper 95%'].item()}}}) & "
+            f"\\num{{{current_results['z'].item()}}} & \\num[round-mode=figures,round-precision=3]{{{current_results['p'].item()}}} \\\\ \n"
+        )
 f.write("\\bottomrule \n") 
 f.write("\end{tabular}\n")
 f.write("\caption{FL between pairs of centers.}\n")
@@ -56,5 +57,3 @@ f.write("\label{tab:fl_pairs}\n")
 f.write("\end{table}\n")
 f.close()
 os.system("cat table_pairs.tex")
-
-
---
+++ test.py
@@ -0,0 +1,20 @@
+import _blake2
+from bla import blo
+
+
+class whatev():
+    def __init__(self):
+        self.x = 42
+    def _get_stuff(self):
+        return self.x
+    
+
+class whatev():
+    def __init__(self):
+        self.x = 42
+    def _get_stuff(self):
+        return self.x
+    
+
+
+
