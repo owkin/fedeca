9ca4311f1be8972e5df2c368a16b6b8d25036971
jeandut <jean.du-terrail@owkin.com>
2025-01-24 15:07:36 +0100

    Update fedeca/utils/fedeca_graphs/clean_log_file.py

--- fedeca/utils/fedeca_graphs/clean_log_file.py
+++ fedeca/utils/fedeca_graphs/clean_log_file.py
@@ -1,6 +1,6 @@
 """Clean the raw workflow file produced by the tracing."""
 # All utils here were originally written by Ulysse Marteau @umarteauowkin for
-# fed-pydeseq2 (https://github.com/owkin/fed-pydeseq2) and are reproduced here with
+# fed-pydeseq2 (https://github.com/owkin/fedpydeseq2-paper-experiments) and are reproduced here with
 # his permissions.
 import pathlib
 import re
