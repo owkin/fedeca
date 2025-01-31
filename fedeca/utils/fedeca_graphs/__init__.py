77e36335f92c8ed550a81ca43cd14d6b3031871f
jeandut <jean.du-terrail@owkin.com>
2025-01-31 14:25:42 +0100

    linting

--- fedeca/utils/fedeca_graphs/__init__.py
+++ fedeca/utils/fedeca_graphs/__init__.py
@@ -1,4 +1,4 @@
 """Graph utils for creating workflow graphs and tables."""
 # All utils here were originally written by Ulysse Marteau @umarteauowkin for
-# fed-pydeseq2 (https://github.com/owkin/fedpydeseq2-paper-experiments) 
+# fed-pydeseq2 (https://github.com/owkin/fedpydeseq2-paper-experiments)
 # and are reproduced here with his permissions.
--- fedeca/utils/fedeca_graphs/clean_log_file.py
+++ fedeca/utils/fedeca_graphs/clean_log_file.py
@@ -1,6 +1,6 @@
 """Clean the raw workflow file produced by the tracing."""
 # All utils here were originally written by Ulysse Marteau @umarteauowkin for
-# fed-pydeseq2 (https://github.com/owkin/fedpydeseq2-paper-experiments) 
+# fed-pydeseq2 (https://github.com/owkin/fedpydeseq2-paper-experiments)
 # and are reproduced here with his permissions.
 import pathlib
 import re
